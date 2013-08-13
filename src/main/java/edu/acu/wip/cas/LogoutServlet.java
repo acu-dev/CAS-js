6469757a87097e22c7d0759ca123dbaa41fcd778
Harvey McQueen <hmcqueen@gmail.com>
2013-08-13 10:09:45 -0500

    MYIPHONE-166

--- src/main/java/edu/acu/wip/cas/LogoutServlet.java
+++ src/main/java/edu/acu/wip/cas/LogoutServlet.java
@@ -1,24 +1,33 @@
 package edu.acu.wip.cas;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author hgm02a
  */
 public class LogoutServlet extends HttpServlet {
+	
+	private final String logoutUrl;
+	
+	@Inject
+	public LogoutServlet(@Named("logoutUrl") String logoutUrl) {
+		super();
+		this.logoutUrl = logoutUrl;
+	}
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		if (req.getSession(false) != null) {
 			req.getSession(false).invalidate();
 		}
-		resp.sendRedirect("/");
+		resp.sendRedirect(this.logoutUrl);
   }
   
 }
